099cdb7c9d7ff9dcfbc519634ae290d38ab5acef
Mekha Matta <mekha.r99@gmail.com>
2022-10-24 20:08:58 +0300

    pthyon files

---
+++ new_proj/escape.py
@@ -0,0 +1,10 @@
+SplitString = "This string has been\n split over\nseveral\nLines"
+print(SplitString)
+
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The Pet shop owner said "NO, no, \'e\'s uh,...he\'s resting".')
+print("The Pet shop owner said \"NO, no, 'e's uh,...he\'s resting\".")
+print("""The Pet shop owner said "NO, no, 'e's uh,...he's resting". """)
---
+++ new_proj/string.py
@@ -0,0 +1,9 @@
+print("Today is a nice to day to learn Python")
+print('Python is fun')
+print("Python's string is easy to use")
+print('we can even include "quotes" in strings')
+print("Hello" + " World")
+greetings = "Hello"
+name = input("please enter your name : ")
+account = "22134151561561561"
+print(greetings + ' ' + name + ' ' + account)
